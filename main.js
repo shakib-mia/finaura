4f17eac411d67ca4bf2d30d2a8e8af3d8ccb7402
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-09-03 13:17:16 +0600

    done

--- main.js
+++ main.js
@@ -45,3 +45,61 @@ const swiper = new Swiper(".swiper", {
   //   },
   // },
 });
+
+document.getElementById("year").innerText = new Date().getFullYear();
+function createScrollDirectionTracker() {
+  let scrollDirection = "up";
+  let lastScrollY = 0;
+
+  function handleScroll() {
+    const currentScrollY = window.pageYOffset;
+
+    if (currentScrollY > lastScrollY) {
+      scrollDirection = "down";
+    } else {
+      scrollDirection = "up";
+    }
+
+    lastScrollY = currentScrollY;
+    // console.log(currentScrollY);
+    if (scrollDirection === "up") {
+      document.getElementById("navbar-container").style.top = "0";
+      document.getElementById("navbar-container").style.transition =
+        "all 0.5s ease";
+      //   document
+      //     .getElementById("navbar-container")
+      //     .classList.add("shadow-[0_0_80px_0_#2B245D21]");
+    } else {
+      document.getElementById("navbar-container").style.top = "-8rem";
+      document.getElementById("navbar-container").style.transition =
+        "all 0.5s ease";
+    }
+
+    if (currentScrollY > 0) {
+      document.getElementById("navbar-container").style.boxShadow =
+        "0 0 20px 0 #2B245D21";
+
+      document.getElementById("navbar-container").style.backgroundColor =
+        "#ffffff";
+    } else {
+      document.getElementById("navbar-container").style.boxShadow = "none";
+      document.getElementById("navbar-container").style.backgroundColor =
+        "#ffffff00";
+    }
+  }
+  // console.log(scrollDirection);
+
+  window.addEventListener("scroll", handleScroll);
+
+  return {
+    getScrollDirection: () => scrollDirection,
+    cleanup: () => {
+      window.removeEventListener("scroll", handleScroll);
+    },
+  };
+}
+
+createScrollDirectionTracker();
+
+document.getElementById("navbar-container").style.height =
+  document.getElementById("navbar-content").clientHeight + "px";
