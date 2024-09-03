b9322560a91b86f5de4ef7bd7a467e9b36d5eb4f
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-09-03 11:04:36 +0600

    till testimonial section done

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Aos
 
+// import Swiper from "swiper";
+
+import { Swiper } from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+// const Swiper = require("https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs");
+
 // Lenis
 
 const lenis = new Lenis();
@@ -14,3 +19,29 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 AOS.init();
+
+// Swiper
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 2000,
+  },
+  navigation: {
+    prevEl: ".slide-left",
+    nextEl: ".slide-right",
+  },
+  slidesPerView: 1,
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 0,
+  //   },
+  //   768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 30,
+  //   },
+  //   1024: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //   },
+  // },
+});
